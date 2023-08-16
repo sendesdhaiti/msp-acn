19861e0932673d11f01be92c31599b7982743707
sendesdhaiti <sdhaiti.business@gmail.com>
2023-08-16 14:15:53 -0400

    update

--- LIBRARIES/REPO/EMAIL/EMAIL.cs
+++ LIBRARIES/REPO/EMAIL/EMAIL.cs
@@ -94,7 +94,6 @@ namespace REPO.EMAIL
         actions.Imsactions actions;
 
 
-
         static bool mailSent = false;
 
         private static void SendCompletedCallback(object sender, AsyncCompletedEventArgs e)
